26b083e21c44ccc16384638674b0001be0c89b98
ski <ski@skis-MacBook-Pro.local>
2022-03-29 17:34:24 +0300

    modified challenges value field to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_28_195107) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_29_143202) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_28_195107) do
     t.datetime "updated_at", null: false
     t.text "contract_abi"
     t.string "setup_address"
-    t.integer "value"
+    t.string "value"
   end
 
   create_table "instances", force: :cascade do |t|
