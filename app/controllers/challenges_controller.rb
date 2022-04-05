34c3bab764d1fa6d4486166b6c1f551f4f9e8881
ski <ski@skis-MacBook-Pro.local>
2022-04-05 15:14:07 +0300

    added setup_code attribute to challenges

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -34,7 +34,7 @@ class ChallengesController < ApplicationController
             difficulty: params['difficulty'], address: params['address'], author: params['author'],
             author_github: params['author_github'], author_email: params['author_email'],
             contract_abi: params['contract_abi'], setup_address: params['setup_address'].upcase,
-            value: params['value'], verified_url: params["verifiedUrl"]
+            value: params['value'], verified_url: params["verifiedUrl"], setup_code: html_cleaner(params["setup_code"])
         )
 
         if @challenge.save
