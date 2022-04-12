90796801bd53b80b5bac0f650fd577d9fe630983
ski <ski@skis-MacBook-Pro.local>
2022-04-12 15:45:30 +0300

    added rank to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_11_133430) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_12_124447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_11_133430) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description_title"
+    t.integer "rank_of_appearance"
   end
 
   create_table "skills", force: :cascade do |t|
