c001be342f98eab4198e531c02d9deba5c5b3172
ski <ski@skis-MacBook-Pro.local>
2022-04-13 20:34:14 +0300

    cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,11 +5,11 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
+# Rails.application.config.action_controller.forgery_protection_origin_check = false
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    origins ["https://inquisitive-faun-0282cb.netlify.app",'*']
+    resource '*', headers: :any, methods: [:get, :post, :options]
   end
 end
